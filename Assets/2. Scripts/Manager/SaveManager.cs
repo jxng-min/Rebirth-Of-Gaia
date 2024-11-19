8f4ab210dcf0fd62a797f526169aa74bb5e712d0
Timothy-KJM <vwmartin@naver.com>
2024-11-19 14:52:26 +0900

    feat: 캐릭터 선택 기능 추가

--- Assets/2. Scripts/Manager/SaveManager.cs
+++ Assets/2. Scripts/Manager/SaveManager.cs
@@ -7,29 +7,39 @@ using System;
 
 namespace Jongmin
 {
-    public class DataManager : MonoBehaviour
+    public class SaveManager : MonoBehaviour
     {
         private PlayerData m_now_player = new PlayerData();
         private string m_save_path;
 
         private void Start()
         {
-            m_save_path = Application.persistentDataPath + "/Save";
+            m_save_path = Application.persistentDataPath + "/PlayerData.txt";
 
             Debug.Log($"{m_save_path} 위치로 플레이어 데이터 저장 경로를 설정하였습니다.");
+
+            if(File.Exists(m_save_path))
+            {
+                LoadData();
+            }
+            else
+            {
+                m_now_player.m_character_type = GameManager.Instance.CharacterType;
+                SaveData();
+            }
         }
 
         public void SaveData()
         {
             string data = JsonUtility.ToJson(m_now_player);
-            File.WriteAllText(m_save_path + "PlayerData", data);
+            File.WriteAllText(m_save_path, data);
 
             Debug.Log($"{m_save_path} 경로로 플레이어 데이터를 저장하는 데 성공하였습니다.");
         }
 
         public void LoadData()
         {
-            string data = File.ReadAllText(m_save_path + "PlayerData");
+            string data = File.ReadAllText(m_save_path);
             m_now_player = JsonUtility.FromJson<PlayerData>(data);
 
             Debug.Log($"{m_save_path} 경로에서 플레이어 데이터를 불러오는 데 성공하였습니다.");
