d5a5b55286d13d64b4e214436f9c6a2df0e8494f
Timothy-KJM <vwmartin@naver.com>
2024-11-21 13:28:52 +0900

    feat: 플레이어 스탯 UI 추가

--- Assets/2. Scripts/Data/PlayerData.cs
+++ Assets/2. Scripts/Data/PlayerData.cs
@@ -1,20 +1,25 @@
-public class PlayerData
-{
-    public Character m_character_type;
-    public playerStatus m_player_status;
-    public int m_talk_id;
-    public int m_talk_action_id;
-    public int m_quest_id;
-    public int m_quest_action_id;
 
-    public void SetPlayerData()
+namespace Jongmin
+{
+    [System.Serializable]
+    public class PlayerData
     {
-        m_character_type = new Character();
-        m_player_status = new playerStatus();
-        
-        m_talk_id = 0;
-        m_talk_action_id = 0;
-        m_quest_id = 1000;
-        m_quest_action_id = 0;
+        public Character m_character_type;
+        public PlayerStatus m_player_status;
+        public int m_talk_id;
+        public int m_talk_action_id;
+        public int m_quest_id;
+        public int m_quest_action_id;
+
+        public PlayerData(Character char_type, PlayerStatus player_status)
+        {
+            m_character_type = char_type;
+            m_player_status = player_status;
+            
+            m_talk_id = 0;
+            m_talk_action_id = 0;
+            m_quest_id = 0;
+            m_quest_action_id = 0;
+        }
     }
 }
--- Assets/2. Scripts/Data/PlayerStatus.cs
+++ Assets/2. Scripts/Data/PlayerStatus.cs
@@ -1,7 +1,14 @@
-public struct playerStatus
+[System.Serializable]
+public class PlayerStatus
 {
-    int m_hp;
-    int m_mp;
-    int m_str;
-    int m_def;
+    public float m_attack_power;
+    public float m_magic_power;
+    public float m_social_influence;
+
+    public PlayerStatus(float attack_power, float magic_power, float social_influence)
+    {
+        m_attack_power = attack_power;
+        m_magic_power = magic_power;
+        m_social_influence = social_influence;
+    }
 }
