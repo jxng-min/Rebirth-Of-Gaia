67cb5cfa352db98f8b0c264f1903942aab516263
Timothy-KJM <vwmartin@naver.com>
2024-12-02 01:38:27 +0900

    fetch: 안드로이드 성능 최적화1

--- Assets/2. Scripts/Ctrl/ButtonCtrl.cs
+++ Assets/2. Scripts/Ctrl/ButtonCtrl.cs
@@ -7,16 +7,15 @@ namespace Jongmin
     {
         private PlayerCtrl m_player_ctrl;
 
-        private void Update()
-        {
-            if (m_player_ctrl == null)
-            {
-                m_player_ctrl = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerCtrl>();
-            }
-        }
-
         public void UpArrowClick()
         {
+            if(m_player_ctrl == null)
+            {
+                Debug.Log("PlayerCtrl이 없으므로 태그로 찾아보고 있습니다.");
+                m_player_ctrl = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerCtrl>();
+                Debug.Log("PlayerCtrl을 찾는 데 성공했습니다.");
+            }
+
             if(GameManager.Instance.GameStatus == "Playing")
             {
                 m_player_ctrl.PlayerJump();
@@ -25,6 +24,13 @@ namespace Jongmin
 
         public void DownArrowClick()
         {
+            if(m_player_ctrl == null)
+            {
+                Debug.Log("PlayerCtrl이 없으므로 태그로 찾아보고 있습니다.");
+                m_player_ctrl = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerCtrl>();
+                Debug.Log("PlayerCtrl을 찾는 데 성공했습니다.");
+            }
+
             if(GameManager.Instance.GameStatus == "Playing")
             {
                 m_player_ctrl.PlayerDown();
@@ -33,12 +39,26 @@ namespace Jongmin
 
         public void ArrowUp()
         {
+            if(m_player_ctrl == null)
+            {
+                Debug.Log("PlayerCtrl이 없으므로 태그로 찾아보고 있습니다.");
+                m_player_ctrl = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerCtrl>();
+                Debug.Log("PlayerCtrl을 찾는 데 성공했습니다.");
+            }
+
             m_player_ctrl.MoveVector = Vector2.zero;
             m_player_ctrl.PlayerStop();
         }
 
         public void Skill1Click()
         {
+            if(m_player_ctrl == null)
+            {
+                Debug.Log("PlayerCtrl이 없으므로 태그로 찾아보고 있습니다.");
+                m_player_ctrl = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerCtrl>();
+                Debug.Log("PlayerCtrl을 찾는 데 성공했습니다.");
+            }
+
             if(GameManager.Instance.GameStatus == "Playing")
             {
                 m_player_ctrl.m_player_skills[0].Effect();
@@ -47,6 +67,13 @@ namespace Jongmin
 
         public void Skill2Click()
         {
+            if(m_player_ctrl == null)
+            {
+                Debug.Log("PlayerCtrl이 없으므로 태그로 찾아보고 있습니다.");
+                m_player_ctrl = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerCtrl>();
+                Debug.Log("PlayerCtrl을 찾는 데 성공했습니다.");
+            }
+
             if(GameManager.Instance.GameStatus == "Playing")
             {
                 m_player_ctrl.m_player_skills[1].Effect();
@@ -55,6 +82,13 @@ namespace Jongmin
 
         public void Skill3Click()
         {
+            if(m_player_ctrl == null)
+            {
+                Debug.Log("PlayerCtrl이 없으므로 태그로 찾아보고 있습니다.");
+                m_player_ctrl = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerCtrl>();
+                Debug.Log("PlayerCtrl을 찾는 데 성공했습니다.");
+            }
+
             if(GameManager.Instance.GameStatus == "Playing")
             {
                 m_player_ctrl.m_player_skills[2].Effect();
