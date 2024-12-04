f70437ebd282a452ba18266e760c65abc16ff48c
Timothy-KJM <vwmartin@naver.com>
2024-12-04 23:30:20 +0900

    feat: Frame Checker 추가

---
+++ Assets/2. Scripts/FrameChecker.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System.Collections;
+
+public class FrameChecker : MonoBehaviour
+{
+    float deltaTime = 0.0f;
+    GUIStyle style;
+    Rect rect;
+    float msec;
+    float fps;
+    float worstFps=100f;
+    string text;
+
+    void Awake() 
+    {
+        int w = Screen.width, h = Screen.height;
+        rect = new Rect(400, 0, w, h * 4 / 100);
+        style = new GUIStyle();
+        style.alignment = TextAnchor.UpperLeft;
+        style.fontSize = h * 4 / 100;
+        style.normal.textColor = Color.cyan;
+
+        StartCoroutine ("worstReset"); 
+    }
+
+    IEnumerator worstReset()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(15f);
+            worstFps = 100f;
+        }
+    }
+
+    void Update()
+    {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+    }
+
+    void OnGUI()
+    {
+        msec = deltaTime * 1000.0f;
+        fps = 1.0f / deltaTime;
+        if(fps < worstFps)
+            worstFps = fps;
+        
+        text = msec.ToString("F1") + "ms (" + fps.ToString("F1") + ") // worst: " + worstFps.ToString("F1");
+        GUI.Label(rect, text, style);
+    }
+}
