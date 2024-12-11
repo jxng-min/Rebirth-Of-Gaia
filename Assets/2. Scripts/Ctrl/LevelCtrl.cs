9bd5817f1f135537ca83a9d5b381b9c77748f317
Timothy-KJM <vwmartin@naver.com>
2024-12-11 15:34:06 +0900

    fetch: 레벨, 설정 UI 임시 변경

---
+++ Assets/2. Scripts/Ctrl/LevelCtrl.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace Jongmin
+{
+    public class LevelCtrl : MonoBehaviour
+    {
+        [Header("SaveManager")]
+        [SerializeField]
+        private SaveManager m_save_manager;
+        
+        [Header("LV / EXP UI")]
+        [SerializeField]
+        private Slider m_exp_bar;
+        [SerializeField]
+        private TMP_Text m_level_text;
+        [SerializeField]
+        private TMP_Text m_exp_text;
+
+        private void OnEnable()
+        {
+            float current_exp = (float)m_save_manager.Player.m_player_status.m_current_exp;
+            float max_exp = (float)ExpData.m_exps[m_save_manager.Player.m_player_status.m_current_level - 1];
+
+            m_level_text.text = $"Lv.{m_save_manager.Player.m_player_status.m_current_level.ToString()}";
+            m_exp_text.text = $"({m_save_manager.Player.m_player_status.m_current_exp} / {ExpData.m_exps[m_save_manager.Player.m_player_status.m_current_level - 1]})"; 
+
+            m_exp_bar.value =  current_exp / max_exp;
+        }
+    }
+}
--- Assets/2. Scripts/Ctrl/SoundSettingCtrl.cs
+++ Assets/2. Scripts/Ctrl/SoundSettingCtrl.cs
@@ -2,65 +2,70 @@ using Jongmin;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SoundValue : MonoBehaviour
+public class SoundSettingCtrl : MonoBehaviour
 {
-    [SerializeField]
-    private Slider m_bgm_slider;
-    [SerializeField]
-    private Slider m_effect_slider;
+    [Header("Save Manager")]
     [SerializeField]
     private SaveManager m_save_manager;
 
+    [Header("Sound Setting UI")]
+    [SerializeField]
+    private Slider m_bgm_slider;
+
+    [SerializeField]
+    private Slider m_effect_slider;
+
     private void Start()
     {
-        Invoke("LateStart", 0.1f);
-    }
-    private void LateStart()
-    {
-        if(SoundManager.Instance != null)
-        {
-            m_bgm_slider.value = SoundManager.Instance.BgmVolume;
-            m_effect_slider.value = SoundManager.Instance.EffectVolume;
-        }
+        m_bgm_slider.value = SoundManager.Instance.BgmVolume;
+        m_effect_slider.value = SoundManager.Instance.EffectVolume;
 
         m_bgm_slider.onValueChanged.AddListener(OnBgmVolumeChanged);
         m_effect_slider.onValueChanged.AddListener(OnEffectVolumeChanged);
-        
-        Debug.Log($"bgm:{m_save_manager.Player.m_bgm_volume},effect:{m_save_manager.Player.m_effect_volume}");
+
         OnBgmVolumeChanged(m_save_manager.Player.m_bgm_volume);
         OnEffectVolumeChanged(m_save_manager.Player.m_effect_volume);
     }
-    public void OnBgmVolumeChanged(float value)
+
+    private void OnBgmVolumeChanged(float value)
     {
         if(SoundManager.Instance != null)
         {
             SoundManager.Instance.BgmVolume = value;
+
             if(m_save_manager.Player == null)
             {
-                Debug.Log($"m_save_manager.Player is null");
-
+                Debug.Log("사운드의 볼륨을 참조할 플레이어가 null입니다.");
+                return;
             }
+
             m_save_manager.Player.m_bgm_volume = value;
             m_save_manager.SaveData();
-            Debug.Log($"BgmVolume = {value}조절 완료");
         }
-        else{
-            Debug.Log($"BgmVolume = {value}조절 실패");
+        else
+        {
+            Debug.LogError("SoundManager가 null이라 백그라운드 볼륨을 조절할 수 없습니다.");
         }
     }
-    public void OnEffectVolumeChanged(float value)
+
+    private void OnEffectVolumeChanged(float value)
     {
         if(SoundManager.Instance != null)
         {
             SoundManager.Instance.EffectVolume = value;
+
+            if(m_save_manager.Player == null)
+            {
+                Debug.Log("사운드의 볼륨을 참조할 플레이어가 null입니다.");
+                return;
+            }
+            
             m_save_manager.Player.m_effect_volume = value;
             m_save_manager.SaveData();
-            Debug.Log($"EffectVolume = {value}조절 완료");
         }
-        else{
-            Debug.Log($"EffectVolume = {value}조절 실패");
+        else
+        {
+            Debug.LogError("SoundManager가 null이라 이펙트 볼륨을 조절할 수 없습니다.");
         }
     }
 }
-
-
