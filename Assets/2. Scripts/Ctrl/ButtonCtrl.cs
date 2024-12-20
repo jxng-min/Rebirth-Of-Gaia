ab135e0c082b7cc007430dbb9afefa6bbb98047e
wnsdud3095 <wnsdud30954@naver.com>
2024-12-20 15:34:58 +0900

    refact : 스킬 쿨타임 관리를 별도 스크립트 사용이 아닌 ButtonCtrl.cs 에서 처리
    투명 이미지로 쿨타임을 표시 하면서 버튼을 비활성화 시켜서 쿨타임 동안 버튼을 추가로 클릭하지 못하게 쿨타임 관리

--- Assets/2. Scripts/Ctrl/ButtonCtrl.cs
+++ Assets/2. Scripts/Ctrl/ButtonCtrl.cs
@@ -10,11 +10,13 @@ namespace Jongmin
     public class ButtonCtrl : MonoBehaviour
     { 
         [Header("Hide Skill Images")]
-        [SerializeField] private Image[] m_hide_images;
+        [SerializeField] 
+        private Image[] m_hide_images;
+
+        [SerializeField]
+        private Button[] m_skill_button;
 
         private PlayerCtrl m_player_ctrl;
-
-
 
         public void TitleClick()
         {
@@ -102,7 +104,7 @@ namespace Jongmin
             if (GameManager.Instance.GameStatus == "Playing")
             {
                 m_player_ctrl.PlayerUseSkill1();
-                StartCoroutine(CoolDownImageCorutine(m_hide_images[0], m_player_ctrl.m_player_skills[0].m_skill_cool_time));
+                StartCoroutine(CoolDownImageCorutine(m_hide_images[0], 0));
             }
         }
 
@@ -118,7 +120,7 @@ namespace Jongmin
             if (GameManager.Instance.GameStatus == "Playing")
             {
                 m_player_ctrl.PlayerUseSkill2();
-                StartCoroutine(CoolDownImageCorutine(m_hide_images[1], m_player_ctrl.m_player_skills[1].m_skill_cool_time));
+                StartCoroutine(CoolDownImageCorutine(m_hide_images[1], 1));
 
             }
         }
@@ -135,12 +137,14 @@ namespace Jongmin
             if (GameManager.Instance.GameStatus == "Playing")
             {
                 m_player_ctrl.PlayerUseSkill3();
-                StartCoroutine(CoolDownImageCorutine(m_hide_images[2], m_player_ctrl.m_player_skills[2].m_skill_cool_time));
+                StartCoroutine(CoolDownImageCorutine(m_hide_images[2], 2));
 
             }
         }
-        public IEnumerator CoolDownImageCorutine(Image image, float cool_time)
+        public IEnumerator CoolDownImageCorutine(Image image, int skill_index)
         {
+            float cool_time = m_player_ctrl.m_player_skills[skill_index].m_skill_cool_time;
+            m_skill_button[skill_index].interactable = false;
             image.gameObject.SetActive(true);
             float now_time = cool_time;
             while (now_time > 0)
@@ -150,7 +154,9 @@ namespace Jongmin
                 yield return null;
             }
             image.fillAmount = 0f;
-            image.gameObject.SetActive(false);          
+            image.gameObject.SetActive(false);
+            m_skill_button[skill_index].interactable = true;
+
         }
     }
 }
--- Assets/2. Scripts/Strategy/Socia/SociaSkill1.cs
+++ Assets/2. Scripts/Strategy/Socia/SociaSkill1.cs
@@ -4,34 +4,14 @@ using UnityEngine;
 
 public class SociaSkill1 : MonoBehaviour,Skill
 {
-    // 효과 : 본인의 공격력과 방어력을 10초간 20% 상승
-    // 발동 조건 : 사회력 20 이상
-    // 쿨타임 3초
-    private SkillCoolDownCtrl m_skill_cool_cown_ctrl;
     public float m_skill_cool_time { get; set; } = 10f;
     private float m_skill_duration = 5f;
     private float m_increase_value = 0.2f;
-
     public void Effect()
     {
-        if(!m_skill_cool_cown_ctrl)
-        {
-            m_skill_cool_cown_ctrl = gameObject.AddComponent<SkillCoolDownCtrl>();
-        }
-
-        if(m_skill_cool_cown_ctrl.m_is_ready)
-        {
-            Debug.Log("소셔가 연대의 외침을 사용한다.");
-            StartCoroutine(GetStrength(m_skill_duration));
-            StartCoroutine(m_skill_cool_cown_ctrl.CoolDownCoroutine(m_skill_cool_time));
-
-        }
-        else
-        {
-            Debug.Log("연대의 외침이 준비되지 않았다.");
-        }
+         Debug.Log("소셔가 연대의 외침을 사용한다.");
+         StartCoroutine(GetStrength(m_skill_duration));
     }
-
     private IEnumerator GetStrength(float time)
     {
         float increase_strength = SaveManager.Instance.Player.m_player_status.m_strength * m_increase_value;
@@ -41,5 +21,4 @@ public class SociaSkill1 : MonoBehaviour,Skill
         SaveManager.Instance.Player.m_player_status.m_strength -= increase_strength;
         Debug.Log($"소셔의 공격력이 다시 감소 :{SaveManager.Instance.Player.m_player_status.m_strength}");
     }
-
 }
