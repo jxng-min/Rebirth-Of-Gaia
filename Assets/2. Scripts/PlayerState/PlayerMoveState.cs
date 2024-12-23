4c104e0876cc37ea2f8f2cc40a97daabfc104406
Timothy-KJM <vwmartin@naver.com>
2024-12-23 15:09:03 +0900

    feat: 플레이어 스테이터스 그래프 추가

--- Assets/2. Scripts/PlayerState/PlayerMoveState.cs
+++ Assets/2. Scripts/PlayerState/PlayerMoveState.cs
@@ -1,4 +1,5 @@
 using Jongmin;
+using System.Collections;
 using UnityEngine;
 
 namespace Junyoung
@@ -14,7 +15,23 @@ namespace Junyoung
                 m_player_ctrl = player_ctrl;
             }
             
+            if(!m_player_ctrl.IsMove)
+            {
+                StartCoroutine(MoveSE());
+            }
+            
             GetComponent<Animator>().SetBool("IsMove", true);
+        }
+
+        private IEnumerator MoveSE()
+        {
+            m_player_ctrl.IsMove = true;
+
+            SoundManager.Instance.PlayEffect($"foot_0{Random.Range(1, 7)}");
+
+            yield return new WaitForSeconds(0.3f);
+
+            m_player_ctrl.IsMove = false;
         }
     }
 }
